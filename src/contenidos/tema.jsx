96a7b8ba6a9fd24704e8aeddaae5149e9530e6ca
Rolgensgerve <olguingerve@gamil.com>
2023-11-09 12:47:26 -0300

    comitiando la parte de sugerencia y contenidos

---
+++ src/contenidos/tema.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './temas.css'
+
+
+
+export default function Temas  ({temas,imagenes}){
+
+    return(
+        <>
+    <div  className="relacionadas">
+     <h4>{temas}</h4> 
+     <div className="secion">
+     <img src={imagenes}  />
+     </div>
+    </div>
+        </>
+        )}
