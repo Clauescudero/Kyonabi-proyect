08f0c76f1e1d0f5c4c5d733aae13c767c0f8e38c
flizanab <flizana63@gmail.com>
2023-11-10 12:09:17 -0300

    cambios medio dia 10-11

---
+++ src/docentes/docentes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Docentes = () => {
+    return (
+        <div className='Docentes'>
+            <h1>Bienvenido al apartado de docentes</h1>
+            <h2>Selecciona una opción</h2>
+            <button>Planificación</button>
+            <button>¡Se un tutor voluntario!</button>
+        </div>
+    );
+};
+
+export default Docentes;
