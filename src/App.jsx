1cdf0e460ba32f31502c0b8ad2661b47d819a280
arutixx <claudio.er123@gmail.com>
2023-11-15 21:20:08 -0300

    se vuelve a version anterior por errores

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import MusicPlayer from './MusicPlayer/MusicPlayer';
 import NavBar from './Reutilizables/NavBar';
 import Home from './Principal/Home';
 import Calendar from './calendar/Calendar';
-<<<<<<< HEAD
-import Temas from "./contenidos/tema.jsx";
-=======
 import DayCell from './calendar/DayCell'; 
->>>>>>> parent of 02c4007 (medio-responsivo)
 import './App.css';
 import Registro from './Registro/Registro';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,20 +16,6 @@ import Footer from "./Footer/Footer";
 import Distribuicion from "./contenidos/distri";
 import Login from "./Login/Login";
 import Docentes from "./docentes/docentes";
-<<<<<<< HEAD
-import FormularioDocentes from "./FormularioDocentes/FormularioDocentes";
-import FormularioEstudiantes from "./FormularioEstudiante/FormularioEstudiante";
-import Perfil from "./Perfil/PerfilEstudiante"
-import Ciencias from "./contenidos-principales/ciencia/ciencias.jsx";
-import Historia from "./contenidos-principales/historia/Historia.jsx";
-import Matematica from "./contenidos-principales/matematicas/matematicas.jsx";
-import Idiomas from "./contenidos-principales/idiomas/idiomas.jsx";
-import Tecnologia from "./contenidos-principales/tecnologia/tecnologia.jsx";
-import Skills from "./contenidos-principales/skills/skills.jsx";
-import Economia from "./contenidos-principales/economia/economia.jsx";
-import Colaboradores from "./colaboradores/colabora.jsx";
-=======
->>>>>>> parent of 02c4007 (medio-responsivo)
 
 function App () {
   const audioSource = 'src/Music';
@@ -43,31 +25,6 @@ function App () {
         <NavBar />
       </header>
       <Routes>
-<<<<<<< HEAD
-      <Route path='/' element={<Home />} />
-      /* <Route path='/registro' element={<Registro />} />
-      <Route path='/Calendar' element={<Calendar />} />
-      <Route path='/Docentes' element={<Docentes />} />
-      <Route path='/Docentes/Planificacion' element={<Planificacion />} />
-      <Route path='/Creatividad' element={<Creatividad />} />
-      <Route path='/sugerencia' element={<Suger />} />
-      <Route path='/contenidos' element={<Distribuicion />} />
-      <Route path='/Formulario-Docentes' element={<FormularioDocentes />} />
-      <Route path='/Formulario-Estudiantes' element={<FormularioEstudiantes />} />
-      <Route path='/Perfil' element={<Perfil />} />
-      <Route path='/Login' element={<Login />} />
-      <Route path='/Temas' element={<Temas />} />
-      <Route path='/formuralioestudiantil' element={<FormularioEstudiantes />} />
-      <Route path="/Formulariodedocente" element={<FormularioDocentes />}/>
-      <Route path="/ciencia" element={<Ciencias />}/>
-      <Route path="/habilidadesblandas" element={<Skills />}/>
-      <Route path="/idiomas" element={<Idiomas/>}/>
-      <Route path="/Tecnologia" element={<Tecnologia/>}/>
-      <Route path="/Historia" element={<Historia/>}/>
-      <Route path="/Economia" element={<Economia/>}/>
-      <Route path="/matematica" element={<Matematica/>}/>
-      <Route path="/Colaboradores"  element={<Colaboradores />} ></Route> */
-=======
         <Route path='/' element={<Home />} />
         <Route path='/registro' element={<Registro />} />
         <Route path='/Calendar' element={<Calendar />} />
@@ -79,7 +36,6 @@ function App () {
         <Route path='/Formulario' element={<Formulario />} />
         
 
->>>>>>> parent of 02c4007 (medio-responsivo)
       </Routes>
       <Footer />
       <MusicPlayer src= {audioSource}></MusicPlayer> */
