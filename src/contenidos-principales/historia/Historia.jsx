4556871b6193d6d351f8c19375599c95acc30c03
Rolgensgerve <olguingerve@gamil.com>
2023-11-16 15:21:29 -0300

    paginas faltan los contenidos

---
+++ src/contenidos-principales/historia/Historia.jsx
@@ -0,0 +1,61 @@
+const Historia = () => {
+    return (
+        <div id="Historia-container" className="contenidos-materiales">
+          <div className='title-creatividad'>
+            <h1>Historia</h1>
+              <p>
+              es la narración de los sucesos del pasado; generalmente los de la humanidad, aunque, también puede no estar centrada en el humano.
+              ​ <br/> Hay quien más breve y concisamente afirma que la historia es el conocimiento del pasado humano.
+                </p>
+          </div>
+          <div className="main-content">
+            <div className="cards-container">
+              <div className="card">
+                <div className="card-content">
+                  <img
+                    className="avatar"
+                    src="https://i.imgur.com/OKS67lhm.jpg"
+                    alt="Aklilu Lemma"
+                  />
+                  <div className="card-text">
+                    <h1>video 1</h1>
+                    <p>Texto descriptivo del video, imagen o enlace</p>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-content">
+                  <img
+                    className="avatar"
+                    src="https://i.imgur.com/OKS67lhm.jpg"
+                    alt="Aklilu Lemma"
+                  />
+                  <div className="card-text">
+                    <h1>video 2</h1>
+                    <p>Texto descriptivo del video, imagen o enlace</p>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-content">
+                  <img
+                    className="avatar"
+                    src="https://i.imgur.com/OKS67lhm.jpg"
+                    alt="Aklilu Lemma"
+                  />
+                  <div className="card-text">
+                    <h1>video 3</h1>
+                    <p>Texto descriptivo del video, imagen o enlace</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <aside className='sidebar'>
+              Recuerda usar nuestro foro si tienes alguna duda o quieres saber más a fondo!
+            </aside>
+          </div>
+        </div>
+      );
+    };
+
+export default Historia;
