5fc4b25d86822eed960767fcaec0fa83e6f1cc67
arutixx <claudio.er123@gmail.com>
2023-11-16 20:43:28 -0300

    enrutado tarjetas

--- src/contenidos/distri.jsx
+++ src/contenidos/distri.jsx
@@ -1,28 +1,41 @@
 import Temas from "./tema";
 import './temas.css'
+import { Link } from "react-router-dom";
 const Distribuicion = ( )=>{
-     
+
 
 
     return(
         <div className="distribuicion">
-     <Temas  temas={"matematica"} imagenes={'https://th.bing.com/th/id/OIP.tcTb-bdm2NP6iweew2VqawHaDp?pid=ImgDet&rs=1'}/>
-      <Temas temas={"tecnologia"}imagenes={'https://cdn.goconqr.com/uploads/media/image/14486436/desktop_db773dd3-0e85-43f0-92e9-5f1da43d39e7.jpg'}/>
-
-      <Temas temas={"ciencia"} imagenes={'https://1.bp.blogspot.com/-8XgMkC75MgU/XpYbmwt4NDI/AAAAAAADBus/SmfmTOcT53gLMz0rE5xMiu3R9pz8WxplACLcBGAsYHQ/s1600/cientifica_Simona+Ceccarelli.jpg'} />
-
-     <Temas  temas={"idiomas"} imagenes={'https://th.bing.com/th/id/OIP.wSlbVneooi-boI1Kmu6JtgHaEK?pid=ImgDet&w=1200&h=675&rs=1'}/>
-
-      <Temas temas={"Historia"} imagenes={'https://metirtahome.files.wordpress.com/2019/02/historia-universal.jpg'}/>
-      <Temas temas={"Recursos"} />
-      <Temas temas={"Economia"} imagenes={'https://conceptodefinicion.de/wp-content/uploads/2019/08/C_Econom%C3%ADa.jpg'} />
-      
-      <Temas temas={"Habilidades blandas"} imagenes={'https://th.bing.com/th/id/OIP.VibUNdd3G8jTD012G3CoDwHaE8?pid=ImgDet&rs=1'} />
-
-      <Temas temas={"creatividad"} imagenes={'https://static.vecteezy.com/system/resources/previews/002/539/385/large_2x/concept-drawing-for-creative-thinking-vector.jpg'} />
-     
+        <Link to="/Matematicas">
+        <Temas  temas={"matematica"} imagenes={'https://th.bing.com/th/id/OIP.tcTb-bdm2NP6iweew2VqawHaDp?pid=ImgDet&rs=1'}/>
+        </Link>
+        <Link to="/Tecnologia">
+        <Temas temas={"tecnologia"}imagenes={'https://cdn.goconqr.com/uploads/media/image/14486436/desktop_db773dd3-0e85-43f0-92e9-5f1da43d39e7.jpg'}/>
+        </Link>
+        <Link to="/Ciencia">
+        <Temas temas={"ciencia"} imagenes={'https://1.bp.blogspot.com/-8XgMkC75MgU/XpYbmwt4NDI/AAAAAAADBus/SmfmTOcT53gLMz0rE5xMiu3R9pz8WxplACLcBGAsYHQ/s1600/cientifica_Simona+Ceccarelli.jpg'} />
+        </Link>
+        <Link to="/Idiomas">
+        <Temas  temas={"idiomas"} imagenes={'https://th.bing.com/th/id/OIP.wSlbVneooi-boI1Kmu6JtgHaEK?pid=ImgDet&w=1200&h=675&rs=1'}/>
+        </Link>
+        <Link to="/Historia">
+        <Temas temas={"Historia"} imagenes={'https://metirtahome.files.wordpress.com/2019/02/historia-universal.jpg'}/>
+        </Link>
+        <Link to="/Recursos">
+        <Temas temas={"Recursos"} />
+        </Link>
+        <Link to="/Economia">
+        <Temas temas={"Economia"} imagenes={'https://conceptodefinicion.de/wp-content/uploads/2019/08/C_Econom%C3%ADa.jpg'} />
+        </Link>
+        <Link to="/Skills">
+        <Temas temas={"Habilidades blandas"} imagenes={'https://th.bing.com/th/id/OIP.VibUNdd3G8jTD012G3CoDwHaE8?pid=ImgDet&rs=1'} />
+        </Link>
+        <Link to="/Creatividad">
+        <Temas temas={"creatividad"} imagenes={'https://static.vecteezy.com/system/resources/previews/002/539/385/large_2x/concept-drawing-for-creative-thinking-vector.jpg'} />
+        </Link>
     
-      </div>
+        </div>
 
 
     )
