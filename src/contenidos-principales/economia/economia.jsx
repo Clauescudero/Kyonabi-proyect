eca1569289c1194869d779bab8dd0b7173ec6725
Mixybc <147181531+Mixybc@users.noreply.github.com>
2023-11-22 01:48:19 -0300

    Agrega contenido en Temas y css

--- src/contenidos-principales/economia/economia.jsx
+++ src/contenidos-principales/economia/economia.jsx
@@ -1,61 +1,73 @@
-const Economia = () => {
-    return (
-        <div id="Economia-container" className="contenidos-materiales">
-          <div className='title-creatividad'>
-            <h1>Economia Personal</h1>
-              <p>
-              La economía personal es la administración de los recursos financieros de una persona para satisfacer sus necesidades y objetivos financieros.
-              <br /> Incluye la planificación financiera, el presupuesto, el ahorro, la inversión y la gestión de la deuda.
-              </p>
-          </div>
-          <div className="main-content">
-            <div className="cards-container">
-              <div className="card">
-                <div className="card-content">
-                  <img
-                    className="avatar"
-                    src="https://i.imgur.com/OKS67lhm.jpg"
-                    alt="Aklilu Lemma"
-                  />
-                  <div className="card-text">
-                    <h1>video 1</h1>
-                    <p>Texto descriptivo del video, imagen o enlace</p>
-                  </div>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-content">
-                  <img
-                    className="avatar"
-                    src="https://i.imgur.com/OKS67lhm.jpg"
-                    alt="Aklilu Lemma"
-                  />
-                  <div className="card-text">
-                    <h1>video 2</h1>
-                    <p>Texto descriptivo del video, imagen o enlace</p>
-                  </div>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-content">
-                  <img
-                    className="avatar"
-                    src="https://i.imgur.com/OKS67lhm.jpg"
-                    alt="Aklilu Lemma"
-                  />
-                  <div className="card-text">
-                    <h1>video 3</h1>
-                    <p>Texto descriptivo del video, imagen o enlace</p>
-                  </div>
-                </div>
+import React from 'react';
+import './economia.css';
+
+const economia = () => {
+  return (
+    <div id="economia-container" className="contenidos-materiales">
+      {/* ... */}
+      <div className='title-economia'>
+        <h1>Economía Personal</h1>
+        <p>
+        Las finanzas personales se refieren al manejo y administración de dinero y bienes de un individuo o familia. Es decir, la administración de todo tu dinero: tus ingresos, tus gastos, bienes materiales, fondos de ahorro, fondos de inversión, seguros, créditos, etc.
+        </p>
+      </div>
+      <div className="main-content-economia">
+        <div className="cards-container-economia">
+          {/* Tarjeta 1 */}
+          <div className="card-economia">
+            <div className="card-content-economia">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/dUiZ5is-Chw"
+                title="Video 1"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+              <div className="card-texteconomia">
+                <h1>La Manera Más Fácil para Entender las Finanzas</h1>
+                <p>Esta es probablemente la manera más fácil para entender cómo funcionan las finanzas personales. Lo hemos explicado con gallinas y huevos.</p>
               </div>
             </div>
-            <aside className='sidebar'>
-              Recuerda usar nuestro foro si tienes alguna duda o quieres saber más a fondo!
-            </aside>
+          </div>
+          {/* Tarjeta 2 */}
+          <div className="card-economia">
+            <div className="card-content-economia">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/ITpM5jVrrkI"
+                title="Título del Video 2"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+              <div className="card-texteconomia">
+                <h1>Organiza tus finanzas</h1>
+                <p>Aprende a tener un control de gastos y cómo mejorar las finanzas personales implementando el sistema de sobres.</p>
+              </div>
+            </div>
+          </div>
+          {/* Tarjeta 3 */}
+          <div className="card-economia">
+            <div className="card-content-economia">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/y4vIMQ6ddso"
+                title="Título del Video 3"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+              <div className="card-texteconomia">
+                <h1>7 hábitos que las personas Ahorrativas hacen a diario</h1>
+                <p>Los que tienen estos 7 hábitos, ahorran mucho dinero. Les compartimos algunas cosas que hacen las personas altamente ahorrativas.</p>
+              </div>
+            </div>
           </div>
         </div>
-      );
-    };
+      </div>
+    </div>
+  );
+};
 
-export default Economia;
+export default economia;
